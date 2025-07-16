4db0705b9940d52b93b730e72e11f30214bf7247
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-16 23:16:48 +0100

    updated script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-alert("Hello from threehouse")
-document.write( "<h1>Welcome to Javascript Basics</h1>"  )
-alert("Thanks for visiting")
+var visitor = prompt('What is your name?')
+var message = 'Hello ' + visitor + ". Welcome to ThreeHouse." + "We are so glad that you came by to visit, " + visitor + ".plesae come again, when you want to learn some more "
+
+document.write(message)
