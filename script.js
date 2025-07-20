9cc9513772d1a929771d6a8268e8da06e22ed9bd
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-20 18:56:55 +0100

    updated the script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var visitor = prompt('What is your name?')
-var message = 'Hello ' + visitor + ". Welcome to ThreeHouse." + "We are so glad that you came by to visit, " + visitor + ".plesae come again, when you want to learn some more "
+var message = 'Hello ' + visitor + ". Welcome to ThreeHouse."
+message += visitor, 
+message += "We are so glad that you came by to visit, " + visitor + ".plesae come again, when you want to learn some more "
 
 document.write(message)
