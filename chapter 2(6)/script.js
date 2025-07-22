3d5955d42592c6b28703f93812fae1cab3c1b6f8
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-22 18:53:21 +0100

    common string method

---
+++ chapter 2(6)/script.js
@@ -0,0 +1,30 @@
+// strings
+console.log('hello world');
+
+
+let email = 'shem@thenetninja.co.uk';
+console.log(email);
+
+//string concatenation
+let firstName = 'Shittu';
+let lastName = 'Emmanuel';
+
+let fullName = firstName + ' ' +  lastName 
+console.log(fullName);
+
+
+// getting characters
+console.log(firstName[2]);
+
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toUpperCase()
+console.log(result, fullName);
+
+let index = email.indexOf('@')
+console.log(index);
