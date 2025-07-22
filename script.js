0c4606a545675c993ae98e70e9b6d839205f53cc
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-22 18:41:19 +0100

    string

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log(1)
-console.log('jjjjj');
+// strings
+console.log('hello world');
+
+
+let email = 'shem@thenetninja.co.uk';
+console.log(email);
+
+//string concatenation
+let firstName = 'Shittu';
+let lastName = 'Emmanuel';
+
+let fullName = firstName + ' ' +  lastName 
+console.log(fullName);
+
+
+// getting characters
+console.log(firstName[2]);
+
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toUpperCase()
+console.log(result, fullName);
