a5e21e9ad266aac00f49f6e9cfed5e81b17a6639
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-23 11:13:03 +0100

    template string

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-// strings
-console.log('hello world');
+// template string
+const title = 'Best reads of 2019';
 
+const author = 'Mario';
 
-let email = 'shem@thenetninja.co.uk';
-console.log(email);
+const likes = 30;
 
-//string concatenation
-let firstName = 'Shittu';
-let lastName = 'Emmanuel';
+// //concatenation way
 
-let fullName = firstName + ' ' +  lastName 
-console.log(fullName);
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 
+// console.log(result);
 
-// getting characters
-console.log(firstName[2]);
+//template string way
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
 
+// console.log(result)
 
-//string length
-console.log(fullName.length);
-
-//string methods
-console.log(fullName.toUpperCase());
-
-let result = fullName.toUpperCase()
-console.log(result, fullName);
-
-let index = email.indexOf('@')
-console.log(index);
+// creating html templates
+let html = `
+   <h2>${title}</h2>
+   <p>By ${author}</p>
+   <span>This blog has ${likes} likes </span>
+`
+console.log(html)
