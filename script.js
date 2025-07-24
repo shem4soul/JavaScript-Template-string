ab14db8b336749deed5a54c1a7c0d1dc47f7371f
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-24 21:06:22 +0100

    type conversion

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
-// template string
-const title = 'Best reads of 2019';
+let ninjas = ['shem', 'dorcas', 'loveth'];
 
-const author = 'Mario';
+// ninjas[1] ='boka'
+// console.log(ninjas[1]);
 
-const likes = 30;
+// let ages = [12, 45, 67, 89, 23];
 
-// //concatenation way
+// console.log(ages[2]);
 
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// let random = ['shaun', 'crystal', 56, 90]
+// console.log(random);
 
+
+// console.log(ninjas.length)
+
+//array methods
+
+// let result = ninjas.join('-')
+// let result = ninjas.indexOf('dorcas');
+// let result = ninjas.concat("blue", 'red');
+// let result = ninjas.push('blue');
+// result = ninjas.pop();
+
+// console.log(ninjas);
+
+
+//booleans & comparisons
+// console.log(true, false, "true", "false");
+
+//methods can returm booleans
+// let email = 'luigi@thenetninja.co.uk';
+// let names = ['mario', 'luigi', 'toad']
+
+// // let result = email.includes('uk');
+// console.log(name == "shaun");
+// // let result = names.includes('luigi')
 // console.log(result);
 
-//template string way
-// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+//comparison operators 
+let age = 25;
 
-// console.log(result)
+// console.log(age == 25);
+// console.log( age == 30);
+// console.log( age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log( age <= 25);
+// console.log(age >= 25);
 
-// creating html templates
-let html = `
-   <h2>${title}</h2>
-   <p>By ${author}</p>
-   <span>This blog has ${likes} likes </span>
-`
-console.log(html)
+
+// let name = 'shaun';
+
+// console.log(name == 'shaun');
+// console.log(name == 'Shaun');
+// console.log(name > 'crystal');
+// console.log(name > 'Shuan');
+// console.log(name > "Ballack");
+
+
+
+// type conversion
+let score = '100';
+
+score = Number(score);
+// console.log(score + 1);
+// console.log((typeof score));
+
+
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(0)
+
+console.log(result);
