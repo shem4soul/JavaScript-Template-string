ce0346688deae91470ef0c6fcb8aaa370cbc9d56
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-05 20:21:01 +0100

    updated for loops

---
+++ chapter 3/sandbox.js
@@ -0,0 +1,17 @@
+//for loops
+
+// for (let index = 0; index < 5; index++) {
+//     console.log('in loop:', index);
+    
+// }
+
+// console.log('Loop Finished');
+
+
+const names = ['shaun', 'mario', 'luigi', 'toad'];  
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+    
+    
+}
