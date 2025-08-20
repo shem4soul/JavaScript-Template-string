b5c1bbff27717e7442bd333dea2c9fc09247a37b
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-20 09:35:00 +0100

    if statement

--- chapter 3/sandbox.js
+++ chapter 3/sandbox.js
@@ -8,17 +8,46 @@
 // console.log('Loop Finished');
 
 
-const names = ['shaun', 'mario', 'luigi', 'toad'];  
+// const names = ['shaun', 'mario', 'luigi', 'toad'];  
 
-// for (let index = 0; index < names.length; index++) {
-//     // console.log(names[index]);
-//     let html = `<div>${names[index]}</div>`
-//     console.log(html);
+// // for (let index = 0; index < names.length; index++) {
+// //     // console.log(names[index]);
+// //     let html = `<div>${names[index]}</div>`
+// //     console.log(html);
+    
+// // }
+
+// let index = 0;
+// while (index < names.length) {
+//     console.log(names[index]);
+//     index++;
+// }
+
+
+// do {
+    
+// } while (condition);
+
+
+// if condition
+
+// const age = 36;
+
+// if (age > 35) {
+//     console.log('you are over 30 years old');
     
 // }
 
-let index = 0;
-while (index < names.length) {
-    console.log(names[index]);
-    index++;
+// const ninjas = ['shaun', 'mario', 'luigi', 'toad'];
+
+// if (ninjas.length > 2){
+//     console.log("that's a lot of ninjas");
+    
+// }
+
+const password = 'passhhhhhhhh';
+
+if (password.length >=8) {
+    console.log('passsord is strong enough');
+    
 }
