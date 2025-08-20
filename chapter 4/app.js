66ec2c616fe402706e3d34505493189af32f9211
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-20 21:07:55 +0100

    updated chaper 4

---
+++ chapter 4/app.js
@@ -0,0 +1,19 @@
+//function declaration
+
+function greet(params) {
+    console.log('Hello there');
+    }
+
+greet();
+greet();
+greet();
+
+
+//function expression
+const speak = function (params) {
+    console.log('good day!');
+    }
+
+speak();
+speak();
+speak();
