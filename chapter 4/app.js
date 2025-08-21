c41658e4f58bfb5c25b649957e5ab7ff76a9c776
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-21 09:04:47 +0100

    arguments and parameters

--- chapter 4/app.js
+++ chapter 4/app.js
@@ -1,19 +1,28 @@
 //function declaration
 
-function greet(params) {
-    console.log('Hello there');
-    }
+// function greet(params) {
+//     console.log('Hello there');
+//     }
 
-greet();
-greet();
-greet();
+// greet();
+// greet();
+// greet();
 
 
-//function expression
-const speak = function (params) {
-    console.log('good day!');
-    }
+// //function expression
+// const speak = function (params) {
+//     console.log('good day!');
+//     }
 
-speak();
-speak();
-speak();
+// speak();
+// speak();
+// speak();
+
+// arguments & parameters
+
+const speak = function(name = 'luigi', time = 'night'){
+    console.log(`good ${time} ${name}`);
+    };
+
+    speak();
+    speak('shaun');
