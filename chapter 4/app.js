e965cfbee9a0e4a2c3ff32c4da4ed384bb404713
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-21 09:49:53 +0100

    arrow function

--- chapter 4/app.js
+++ chapter 4/app.js
@@ -29,9 +29,46 @@
 
 // returning values
 
-const calcArea = function (radius) {
-    return 3.14 * radius**2;
-};
+// const calcArea = function (radius) {
+//     return 3.14 * radius**2;
+// };
+
+// const area = calcArea(5);
+// console.log(area);
+
+//Arrow function 
+
+const calcArea = radius => 3.14 **2;
 
 const area = calcArea(5);
 console.log(area);
+
+//practise arrow function
+
+// const greet = function () {
+//     return 'hellow, world';
+// }
+
+const greet = () => 'hello world'
+
+console.log(greet());
+
+
+// const bill = function (products, tax) {
+//     let total = 0;
+//     for (let index = 0; index < products.length; index++) {
+//        total += products[index] + products[index] * tax;
+//     }
+//     return total;
+// }
+
+
+const bill = (products, tax) => {
+  let total = 0;
+      for (let index = 0; index < products.length; index++) {
+         total += products[index] + products[index] * tax;
+      }
+      return total;
+}
+
+console.log(bill([10,15,30], 0.2));
