dcc52b37ac3680999c32e1705681bad3c91800cf
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-23 04:28:26 +0100

    updated chapter 4

--- chapter 4/app.js
+++ chapter 4/app.js
@@ -1,74 +1,24 @@
-//function declaration
+// function delclaration
 
 // function greet(params) {
-//     console.log('Hello there');
-//     }
+//     console.log(('Hello there!'));
+    
+// }
 
 // greet();
-// greet();
-// greet();
 
 
-// //function expression
 // const speak = function (params) {
-//     console.log('good day!');
-//     }
-
-// speak();
-// speak();
-// speak();
-
-// arguments & parameters
-
-// const speak = function(name = 'luigi', time = 'night'){
-//     console.log(`good ${time} ${name}`);
-//     };
-
-//     speak();
-//     speak('shaun');
-
-// returning values
-
-// const calcArea = function (radius) {
-//     return 3.14 * radius**2;
+//     console.log('Good day!');
 // };
 
-// const area = calcArea(5);
-// console.log(area);
+// speak();
 
-//Arrow function 
+// arguments and parameters
 
-const calcArea = radius => 3.14 **2;
-
-const area = calcArea(5);
-console.log(area);
-
-//practise arrow function
-
-// const greet = function () {
-//     return 'hellow, world';
-// }
-
-const greet = () => 'hello world'
-
-console.log(greet());
-
-
-// const bill = function (products, tax) {
-//     let total = 0;
-//     for (let index = 0; index < products.length; index++) {
-//        total += products[index] + products[index] * tax;
-//     }
-//     return total;
-// }
-
-
-const bill = (products, tax) => {
-  let total = 0;
-      for (let index = 0; index < products.length; index++) {
-         total += products[index] + products[index] * tax;
-      }
-      return total;
+const speak = function (name = 'fejiro', time = 'morning') {
+    console.log(`good ${time}, ${name}`);
+    
 }
 
-console.log(bill([10,15,30], 0.2));
+speak( 'dorcas', 'afternnon');
