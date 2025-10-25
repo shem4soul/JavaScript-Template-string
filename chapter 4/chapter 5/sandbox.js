62b2456c0ded2ac4b246c1154610a74a0a96da87
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-25 10:43:40 +0100

    added ths.word key

--- chapter 4/chapter 5/sandbox.js
+++ chapter 4/chapter 5/sandbox.js
@@ -5,8 +5,25 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.com',
     location: 'berlin',
-    blogs: ['why mac & chesse rules', '10 things to make with marmite']
+    blogs: ['why mac & chesse rules', '10 things to make with marmite'],
+    login: function () {
+        console.log('The user logged in');
+    },
+    logout: function (params) {
+        console.log('The User logged out');
+        
+    },
+    logBlogs: function (params) {
+        console.log('This user has written the following Blogs:');
+       this.blogs.forEach(blog =>{
+        console.log(blog);
+       })      
+    }
 };
+
+user.logBlogs();
+console.log(this);
+
 
 console.log(user);
 console.log(user.name);
@@ -21,3 +38,6 @@ user['name'] = 'chun-li';
 console.log(user['name']);
 
 console.log((typeof user));
+
+user.login();
+user.logout();
