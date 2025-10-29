43bfa98550541c32f1a67cbe36edd473a3ef7d40
Shittu Emmanuel <shem4soul@gmail.com>
2025-10-29 19:22:38 +0100

    added chapter 6 lesson 5

--- JavaScript Tuto Netninja/Chapter 6/sandbox.js
+++ JavaScript Tuto Netninja/Chapter 6/sandbox.js
@@ -11,29 +11,50 @@
 //     console.log(para);
 // })
 
-const error = document.querySelectorAll('.error');
+// const error = document.querySelectorAll('.error');
 
-error.forEach(erro => {
-    console.log(erro);
+// error.forEach(erro => {
+//     console.log(erro);
+// })
+
+// // console.log(errors);
+
+
+
+// //get element by Id
+// const title = document.getElementById('page-title');
+//   console.log(title);
+
+
+//   //get elements by their class name
+//   const errors = document.getElementsByClassName('error');
+//   console.log(errors);
+//   console.log(errors[0]);
+  
+//   //get elements by their tag name
+//   const paras = document.getElementsByTagName('p');
+//   console.log(paras);
+//   console.log(paras[1]);
+  
+  
+
+// const para = document.querySelector('p');
+// // console.log(para.innerText);
+// // para.innerText += ' ninjas are awesome!';
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para =>{
+//      console.log(para.innerText);
+//      para.innerText += ' new text ';
+     
+// })
+
+const content = document.querySelector('.content');
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>This is a new h2</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+  content.innerHTML += `<p>${person}</p>`;
 })
-
-// console.log(errors);
-
-
-
-//get element by Id
-const title = document.getElementById('page-title');
-  console.log(title);
-
-
-  //get elements by their class name
-  const errors = document.getElementsByClassName('error');
-  console.log(errors);
-  console.log(errors[0]);
-  
-  //get elements by their tag name
-  const paras = document.getElementsByTagName('p');
-  console.log(paras);
-  console.log(paras[1]);
-  
-  
